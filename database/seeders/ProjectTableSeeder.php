30edec5749947dcef05e5c3130e70d88246f7abc
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-07-24 17:41:02 +0200

    model - migration - seeder of type

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use App\Models\Project;
+use App\Models\Type;
 
 
 
@@ -19,5 +20,15 @@ class ProjectTableSeeder extends Seeder
     public function run()
     {
         Project :: factory() -> count(10) -> create();
+
+        $projects = Project :: factory() -> count(100) -> make();
+
+        foreach ($projects as $project) {
+
+            $type = Type :: inRandomOrder() -> first();
+
+            $project -> type_id = $type -> id;
+            $project -> save();
     }
+}
 }
